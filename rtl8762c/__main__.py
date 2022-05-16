28eb20273444554861af61f2284eed73f594025c
cyber-murmel <marble@chaoscomputer.club>
2022-05-16 14:19:20 +0200

    added license

--- rtl8762c/__main__.py
+++ rtl8762c/__main__.py
@@ -78,7 +78,22 @@ def parse_arguments():
     parser = ArgumentParser(
         description="rtltool.py - Tool for programming the Realtek RTL8762C SoC",
         prog="rtltool.py",
-        epilog="",
+        epilog="""
+            Copyright (C) 2022  marble
+
+            This program is free software: you can redistribute it and/or modify
+            it under the terms of the GNU General Public License as published by
+            the Free Software Foundation, either version 3 of the License, or
+            (at your option) any later version.
+
+            This program is distributed in the hope that it will be useful,
+            but WITHOUT ANY WARRANTY; without even the implied warranty of
+            MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+            GNU General Public License for more details.
+
+            You should have received a copy of the GNU General Public License
+            along with this program.  If not, see <https://www.gnu.org/licenses/>
+        """,
     )
 
     verbosity = parser.add_mutually_exclusive_group()
